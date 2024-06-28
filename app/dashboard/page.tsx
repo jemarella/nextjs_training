13241f9d2e2be15c3a6eb4ae26437d53f2067c81
jemarella <jesus.arellanes@programadoresjava.mx>
2024-06-28 09:03:26 -0600

    fiveinvoice

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,12 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '@/app/lib/data';
+{/* se cambia por la liena de abajo para usar query por SQL import { fetchRevenue } from '@/app/lib/data'; */}
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data'; {/*se agregan consultas por SQL */}
  
 export default async function Page() {
     const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices(); // Se agrega esta línea para traer las primeras 5 facturas usando SQL query
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -23,7 +25,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         { <RevenueChart revenue={revenue}  /> }
-        {/* <LatestInvoices latestInvoices={latestInvoices}/> */}
+        { <LatestInvoices latestInvoices={latestInvoices}/> }  {/*esraba comentado hasta que se importo fetchLatestInvoices */}
       </div>
     </main>
   );
