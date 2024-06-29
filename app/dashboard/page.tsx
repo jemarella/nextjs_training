c27eeb60847f5b39aa3076db52961c54ee1c8d94
jemarella <jesus.arellanes@programadoresjava.mx>
2024-06-28 19:55:56 -0600

    capitulo8

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,25 +3,41 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 {/* se cambia por la liena de abajo para usar query por SQL import { fetchRevenue } from '@/app/lib/data'; */}
-import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data'; {/*se agregan consultas por SQL */}
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; {/*se agregan consultas por SQL */}
  
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices(); // Se agrega esta línea para traer las primeras 5 facturas usando SQL query
+    const carddata = await fetchCardData();
+
+    const numberOfCustomers = carddata.numberOfCustomers;
+    const numberOfInvoices = carddata.numberOfInvoices;
+    const totalPaidInvoices = carddata.totalPaidInvoices;
+    const totalPendingInvoices = carddata.totalPendingInvoices;
+    
+  {/* otra manera de hacerlo sin declarar carddata 
+    const {
+      numberOfInvoices,
+      numberOfCustomers,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    } = await fetchCardData();
+  */}
+    
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
+        { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
+        { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
+        { <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        /> }
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         { <RevenueChart revenue={revenue}  /> }
